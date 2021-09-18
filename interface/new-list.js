3ac0daa4470e267686da023673cfe418d01b1fbd
Henry Huang <h1886@outlook.com>
2021-09-18 18:06:43 +0800

    cb index service

--- interface/new-list.js
+++ interface/new-list.js
@@ -3,6 +3,13 @@
  */
 const args = process.argv.slice(2);
 const saveToFile = args[0] || false
-
 const GetInitialListBackend = require('../lib/getInitialListBackend')
-new GetInitialListBackend({saveToFile}).run().then(console.log)
+const Logger = require("../lib/logger");
+const {getLogFileName} = require("../lib/utils");
+
+const debug = args['d']
+let logFile = getLogFileName()
+const logger = new Logger(logFile, Boolean(debug))
+new GetInitialListBackend({saveToFile, logger}).run().then(({ csvContent }) => {
+    console.log(csvContent)
+})
